c7585ab5ffa0eaec2b9cc4927cb3ad6e796f107c
Galaxito21 <christopher.19070030@itsmotul.edu.mx>
2023-05-19 09:43:48 -0600

    Ajustes de vistas y componentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,8 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/forms')],
 };

0fb6bccbf5e7c84e14e038802e1647d4c9727d76
ErickBastoCen <erick.19070005@itsmotul.edu.mx>
2023-05-21 23:49:31 -0600

    Personalizado las vistas de las vacantes-sin vacantes

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,34 +1,40 @@
 
 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-    @foreach($vacantes as $vacante)
+    @if(count($vacantes)>0)
+        @foreach($vacantes as $vacante)
 
-        <div class ="p-6 bg-red-200 border-double rounded-lg">
-            <div class="text-center">
-                <a href="#" class="text-xl font-bold">
-                    {{ $vacante -> titulo }}
-                </a>
-                @php
-                    $ultimoDia = \Carbon\Carbon::parse($vacante->ultimo_dia);
-                @endphp
-                <p>Postulaciones antes del: {{ $ultimoDia->format('d/m/Y') }}</p>
-                <p> {{ $vacante -> empresa }}</p>
+            <div class ="p-6 bg-red-200 border-double rounded-lg">
+                <div class="text-center">
+                    <a href="#" class="text-xl font-bold">
+                        {{ $vacante -> titulo }}
+                    </a>
+                    @php
+                        $ultimoDia = \Carbon\Carbon::parse($vacante->ultimo_dia);
+                    @endphp
+                    <p>Postulaciones antes del: {{ $ultimoDia->format('d/m/Y') }}</p>
+                    <p> {{ $vacante -> empresa }}</p>
+                </div>
+                <br>
+                <div class="flex flex-col gap-3 items-stretch ">
+                    <a href="#" class="bg-yellow-400 py-2 px-4 rounded-lg text-black text-xs font-bold text-center">
+                        Candidatos
+                    </a>
+
+                    <a href="#" class="bg-green-600 py-2 px-4 rounded-lg text-white text-xs font-bold text-center">
+                        Editar
+                    </a>
+
+                    <a href="#" class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold text-center">
+                        Eliminar
+                    </a>
+                </div>
             </div>
             <br>
-            <div class="flex flex-col gap-3 items-stretch ">
-                <a href="#" class="bg-yellow-400 py-2 px-4 rounded-lg text-black text-xs font-bold text-center">
-                    Candidatos
-                </a>
 
-                <a href="#" class="bg-green-600 py-2 px-4 rounded-lg text-white text-xs font-bold text-center">
-                    Editar
-                </a>
+        @endforeach
 
-                <a href="#" class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold text-center">
-                    Eliminar
-                </a>
-            </div>
-        </div>
-        <br>
+    @else
+        <p class="text-center text-xl text-black font-bold">Calma, si aun no has registrado nada</p>
 
-    @endforeach
+    @endif
 </div>

c58fab1e2712f154c4750fdb317c8ecfe5512f79
ErickBastoCen <erick.19070005@itsmotul.edu.mx>
2023-05-24 18:49:02 -0600

    Sube sus datos

--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,3 +1,11 @@
-<div>
+<div class="bg-gray-900 p-5 mt-10 flex flex-col justify-center items-center">
+    <h3 class="text-2xl text-center my-4 font-bold">Postulate a la chambita</h3>
+
+    <form clas="w-96 mt-5">
+        <div class="mb-4">
+            <x-label for = "cv" :value="__('Hoja de presentación o CV (PDF)')" />
+            <x-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full"/>
+        </div>
+    </form>
     <p>Form</p>
 </div>
